74b0248bb31e8f437fec9d38a02874b0aec00b3d
kinakomoch <adjqjmu125@gmail.com>
2021-03-25 00:06:12 +0900

    copying samples until organising my thoughts

--- backend/resources/js/app.js
+++ backend/resources/js/app.js
@@ -1,3 +1,7 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import HeaderComponent from "./components/HeaderComponent";
+import TaskListComponent from "./components/TaskListComponent";
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
@@ -20,6 +24,7 @@ window.Vue = require('vue').default;
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('header-component', HeaderComponent);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -27,6 +32,20 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/tasks',
+            name: 'task.list',
+            component: TaskListComponent
+        },
+    ]
+});
+
 const app = new Vue({
     el: '#app',
+    router
 });
